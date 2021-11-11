38a857981bbebbae0174e3cc13c072bcd750efaa
PabloHdzVizcarra <pablourbanohdzvizcarra@gmail.com>
2021-11-10 21:12:58 -0600

    feat create query by fetch by api key

--- src/main/java/jvm/pablohdz/todoapi/repository/TodoRepository.java
+++ src/main/java/jvm/pablohdz/todoapi/repository/TodoRepository.java
@@ -1,11 +1,20 @@
 package jvm.pablohdz.todoapi.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.Collection;
 
 import jvm.pablohdz.todoapi.entity.Todo;
 
 @Repository
 public interface TodoRepository extends JpaRepository<Todo, Long>
 {
+    @Query(
+            nativeQuery = true,
+            value = "SELECT * FROM todo t WHERE t.user_api_key =: apikey"
+    )
+    Collection<Todo> findByApiKey(@Param("apikey") String apikey);
 }
