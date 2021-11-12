1f5c20ad11462073b815d4f6b62394c91970088a
PabloHdzVizcarra <pablourbanohdzvizcarra@gmail.com>
2021-11-11 20:16:45 -0600

    feat create method to controller for delete todo

--- src/main/java/jvm/pablohdz/todoapi/controller/TodoController.java
+++ src/main/java/jvm/pablohdz/todoapi/controller/TodoController.java
@@ -3,13 +3,19 @@ package jvm.pablohdz.todoapi.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.method.P;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Map;
 
 import jvm.pablohdz.todoapi.dto.TodoDto;
 import jvm.pablohdz.todoapi.dto.TodoRequest;
@@ -40,5 +46,12 @@ public class TodoController
     {
         List<TodoDto> todoList = todoService.fetchTodosByApiKey();
         return ResponseEntity.ok(todoList);
+    }
+
+    @PostMapping("/delete")
+    public ResponseEntity<Void> delete(@RequestBody TodoRequest request)
+    {
+        todoService.deleteTodoByName(request.getName());
+        return ResponseEntity.noContent().build();
     }
 }
