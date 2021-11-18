bdfc91013b815fa8355f114a4a7c7f377d264cf4
PabloHdzVizcarra <pablourbanohdzvizcarra@gmail.com>
2021-11-17 21:33:10 -0600

    refactor delete unnecessary code

--- src/main/java/jvm/pablohdz/todoapi/TodoApiApplication.java
+++ src/main/java/jvm/pablohdz/todoapi/TodoApiApplication.java
@@ -2,13 +2,9 @@ package jvm.pablohdz.todoapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Import;
-
-import jvm.pablohdz.todoapi.configuration.SwaggerConfig;
 
 
 @SpringBootApplication
-@Import(SwaggerConfig.class)
 public class TodoApiApplication
 {
     public static void main(String[] args)
